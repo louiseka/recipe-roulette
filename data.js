f4ba71184c7507fa5f46698816fe564d2073eb77
Louise Aldridge <louisealdridge88@gmail.com>
2024-07-01 15:19:40 +0100

    Updating images and links in data js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
-const recipeData = [
+export const recipeData = [
     {
-        recipeTags: ["italian"],
+        recipeTags: ["Italian"],
         quickRecipe: true,
         recipeName: "Carbonara",
         recipeDescription: "The authentic way, without the cream!",
@@ -10,17 +10,17 @@ const recipeData = [
     },
 
     {
-        recipeTags: ["italian"],
+        recipeTags: ["Italian"],
         quickRecipe: false,
         recipeName: "Margherita Pizza",
         recipeDescription: "Sometimes a simple pizza is the best comfort.",
         recipeLink: "https://plumstreetcollective.com/italian-margherita-pizza/",
-        image: "./Assets/Margherita Pizza.jpg",
+        image: "./Assets/Margherita-Pizza.jpg",
         alt: "A margherita pizza displayed on a wooden board"
     },
 
     {
-        recipeTags: ["italian"],
+        recipeTags: ["Italian"],
         quickRecipe: false,
         recipeName: "Tiramisu",
         recipeDescription: "A coffee lovers favourite dessert",
@@ -30,28 +30,28 @@ const recipeData = [
     },
 
     {
-        recipeTags: ["american"],
+        recipeTags: ["American"],
         quickRecipe: false,
         recipeName: "Southern Mac and Cheese",
         recipeDescription: "Cheese and Pasta, what more could you want?",
         recipeLink: "https://www.dinneratthezoo.com/southern-mac-and-cheese/",
-        image: "./Assets/Mac and Cheese.jpg",
+        image: "./Assets/Mac-and-Cheese.jpg",
         alt: "An oven dish containing mac and cheese"
     },
 
 
     {
-        recipeTags: ["american"],
+        recipeTags: ["American"],
         quickRecipe: false,
         recipeName: "Buttermilk Fried Chicken Tenders ",
         recipeDescription: "Not KFC's secret spice mix, but better",
         recipeLink: "https://www.onceuponachef.com/recipes/buttermilk-fried-chicken-tenders-2.html",
-        image: "./Assets/Chicken Tenders.jpg",
+        image: "./Assets/Chicken-Tenders.jpg",
         alt: "A plated of chicken tenders and chips"
     },
 
     {
-        recipeTags: ["japanese"],
+        recipeTags: ["Japanese"],
         quickRecipe: true,
         recipeName: "Ramen",
         recipeDescription: "Fresh veggies and your choice of protein make this extra delicious.",
@@ -61,79 +61,79 @@ const recipeData = [
     },
 
     {
-        recipeTags: ["japanese"],
+        recipeTags: ["Japanese"],
         quickRecipe: true,
         recipeName: "Yaki Udon",
         recipeDescription: "A satisfying quick noodle dish after a long day.",
         recipeLink: "https://drivemehungry.com/yaki-udon-stir-fried-udon-noodles/",
-        image: "./Assets/Yaki Udon.jpg",
+        image: "./Assets/Yaki-Udon.jpg",
         alt: "A plate of Yaki Udon"
     },
 
     {
-        recipeTags: ["french"],
+        recipeTags: ["French"],
         quickRecipe: false,
         recipeName: "Coq au Vin",
         recipeDescription: "A delicious rustic and hearty meal.",
         recipeLink: "https://www.raymondblanc.com/recipes/coq-au-vin/",
-        image: "./Assets/Coq au Vin.jpg",
+        image: "./Assets/Coq-au-Vin.jpg",
         alt: "A casserole dish filled with Coq au Vin"
     },
 
     {
-        recipeTags: ["french"],
+        recipeTags: ["French"],
         quickRecipe: false,
         recipeName: "Potatoes au Gratin",
         recipeDescription: "Potatoes, cream, and cheese. Enough said.",
         recipeLink: "https://www.recipetineats.com/potatoes-au-gratin/",
-        image: "./Assets/Potato Gratin.jpg",
+        image: "./Assets/Potato-Gratin.jpg",
         alt: "An oven dish containing potato gratin with a fresh sprig of rosemary"
     },
 
     {
-        recipeTags: ["english"],
+        recipeTags: ["English"],
         quickRecipe: false,
         recipeName: "Chicken Tikka Masala",
         recipeDescription: "Arguably the most famlous curry in the world.",
         recipeLink: "https://www.krumpli.co.uk/chicken-tikka-masala/",
-        image: "./Assets/Chicken Tikka Masala.jpg",
+        image: "./Assets/Chicken-Tikka-Masala.jpg",
         alt: "A bowl of chicken tikka masala"
     },
 
 
     {
-        recipeTags: ["english"],
+        recipeTags: ["English"],
         quickRecipe: false,
         recipeName: "Toad in the Hole",
         recipeDescription: "Is there anything more comforting?",
         recipeLink: "https://www.tamingtwins.com/toad-in-the-hole-recipe/",
-        image: "./Assets/Toad in the Hole.jpg",
+        image: "./Assets/Toad-in-the-Hole.jpg",
         alt: "A close up of a toad in the hole with a sprig of rosemary on top"
     },
 
     {
-        recipeTags: ["indian"],
+        recipeTags: ["Indian"],
         quickRecipe: false,
         recipeName: "Chicken Biriyani",
         recipeDescription: "Everyone's favourite takeaway dish.",
         recipeLink: "https://www.indianhealthyrecipes.com/chicken-biryani-recipe/",
-        image: "./Assets/Chicken Biriyani.jpg",
+        image: "./Assets/Chicken-Biriyani.jpg",
         alt: "A bowl of Chicken Biriyani with fresh onion and lemon on top"
     },
 
 
     {
-        recipeTags: ["indian"],
+        recipeTags: ["Indian"],
         quickRecipe: true,
         recipeName: "Onion Bahji",
         recipeDescription: "Fried to crispy perfection",
         recipeLink: "https://www.kitchensanctuary.com/onion-bhaji/",
-        image: "./Assets/Onion Bahji.jpg",
+        image: "./Assets/Onion-Bahji.jpg",
         alt: "A bowl of onion bahjis"
     },
 
     {
-        recipeTags: ["korean"],
+        recipeTags: ["Korean"],
         quickRecipe: false,
         recipeName: "Bibimbap",
         recipeDescription: "A beloved Korean dish to many people",
@@ -144,7 +144,7 @@ const recipeData = [
 
 
     {
-        recipeTags: ["korean"],
+        recipeTags: ["Korean"],
         quickRecipe: true,
         recipeName: "Tteokbokki",
         recipeDescription: "There's always room for more of these spicy rice cakes",
